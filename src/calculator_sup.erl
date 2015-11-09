7928244dc67fcb56f6aad41308096eb4875e3552
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2015-11-09 15:35:34 +0200

    empty a repeated structure for calc

---
+++ src/calculator_sup.erl
@@ -0,0 +1,72 @@
+%%%-------------------------------------------------------------------
+%%% @author Yana P. Ribalchenko <yanki@hole.lake>
+%%% @copyright (C) 2015, Yana P. Ribalchenko
+%%% @doc
+%%%       My calculator supervisor
+%%% @end
+%%% Created :  9 Nov 2015 by Yana P. Ribalchenko <yanki@hole.lake>
+%%%-------------------------------------------------------------------
+-module(calculator_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    io:format("calculator_sup start_link/0, pid ~p~n", [self()]),
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    io:format("calculator_sup init, pid ~p~n", [self()]),
+    RestartStrategy = one_for_one,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 3600,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000,
+    Type = worker,
+
+    AChild = {calc_name, {calculator, start_link, []},
+              Restart, Shutdown, Type, [calculator]},
+
+    {ok, {SupFlags, [AChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
